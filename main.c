6341595fa90237f6090ab019aa1b2c6951fbed84
Your Name <you@example.com>
2019-09-30 22:23:19 +0300

    Задание 2а Задание 2b

--- main.c
+++ main.c
@@ -8,6 +8,36 @@ void DecToBin(int n) {
     printf("%d", bit);
 }
 
+int Pow(int a, int b) {
+    int res = 1;
+    for(int i = 0; i < b; i++) {
+        res *= a;
+    }
+    return res;
+}
+
+int PowR(int a, int b) {
+    int res = 1;    
+    if (b != 0) {
+        b--;
+        res = PowR(a, b);
+        res *= a;
+    }
+
+    return res;
+}
+
+int PowR2(int a, int b) {
+    int res = 1;
+    if (b != 0) {
+        b--;
+        res = PowR(a, b);
+        res *= a;
+    }
+
+    return res;
+}
+
 void Task1() {
     int n = 0;
     printf("Введите целое положительное число: ");
@@ -16,11 +46,40 @@ void Task1() {
     DecToBin(n);
 }
 
+void Task2() {
+    int a = 0;
+    int b = 0;
+    printf("Введите целое положительное число: ");
+    scanf("%d", &a);
+    printf("Введите показатель степени: ");
+    scanf("%d", &b);
+
+    printf("Без рекурсии: \n");
+    int res = Pow(a,b);
+    printf("%d в степени %d равно %d\n", a, b, res);
+
+    printf("Рекурсивно: \n");
+    res = PowR(a,b);
+    printf("%d в степени %d равно %d\n", a, b, res);
+
+    printf("Рекурсивно, используя четность степени : \n");
+    res = PowR2(a,b);
+    printf("%d в степени %d равно %d\n\n", a, b, res);
+}
+
 int main() {
     //1. Реализовать функцию перевода из 10 системы в двоичную используя рекурсию.
     printf("Задача 1\n");
     Task1();
-    printf("\n");
+    printf("\n\n");
+    
+    //2. Реализовать функцию возведения числа a в степень b:
+    //a. без рекурсии;
+    //b. рекурсивно;
+    //c. *рекурсивно, используя свойство чётности степени.
+    printf("Задача 2\n");
+    Task2();
+    printf("\n\n");
 
     return 0;
 }
