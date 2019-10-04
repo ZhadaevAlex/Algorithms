143e7fcb1bce43397c47dff5ace77b6422e3037d
Your Name <you@example.com>
2019-10-04 13:18:47 +0300

    Задание 2

--- main.c
+++ main.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 
 void fillArray(int* arr, int len) {
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
         arr[i] = rand() % 100;
-    }
 }
 
 void printArray(int* arr, int len) {
-    for (int i = 0; i < len; i++) {
-
+    for (int i = 0; i < len; i++)
         printf("%d ", arr[i]);
-    }
     printf("\n");
 }
 
@@ -29,10 +26,7 @@ int bubbleSort(int* arr, int len) {
             operations++;
 
             if (arr[j] > arr[j + 1])
-            {
                 swap(&arr[j], &arr[j + 1]);
-
-            }
         }
     }
 
@@ -40,17 +34,35 @@ int bubbleSort(int* arr, int len) {
 }
 
 //Пузырьковый метод (оптимизированный)
-//Оптимизация произведена в двух местах: количество итераций обоих счетчиков
 int bubbleSortOpt(int* arr, int len) {
     int operations = 0;
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = 0; j < len - i; j++) {
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len - 1 - i; j++) {
             operations++;
 
             if (arr[j] > arr[j + 1])
-            {
                 swap(&arr[j], &arr[j + 1]);
-            }
+        }
+    }
+
+    return operations;
+}
+
+//Шейкер
+int shaker(int* arr, int len) {
+    int operations = 0;
+
+    for (int i = 0; i < len / 2; i++) {
+        for (int j = i; j < len - 1 - i; j++) {
+            operations++;
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+
+        for (int j = len - 2 - i; j > i; j--) {
+            operations++;
+            if (arr[j] < arr[j - 1])
+                swap(&arr[j], &arr[j - 1]);
         }
     }
 
@@ -59,23 +71,32 @@ int bubbleSortOpt(int* arr, int len) {
 
 int main(void)
 {
-    //Инициализируем массив
     int size = 9;
+
+    printf("Неоптимизированный пузырьковый метод\n");
     int arr1[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     printArray(arr1, size);
-
-    //Сортируем неоптимизированным пузырьковым методом и получаем кол-во операций
     int oper = bubbleSort(arr1, size);
     printArray(arr1, size);
-    printf("Количество операций: \%d\n", oper);
+    printf("Количество операций: %d\n\n", oper);
 
+    //-------------------------------------------------------------------------------
+
+    printf("Oптимизированный пузырьковый метод\n");
     int arr2[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     printArray(arr2, size);
-
-    //Сортируем оптимизированным пузырьковым методом
     oper = bubbleSortOpt(arr2, size);
     printArray(arr2, size);
-    printf("Количество операций: \%d\n", oper);
+    printf("Количество операций: %d\n\n", oper);
+
+    //-------------------------------------------------------------------------------
+
+    printf("Метод шейкер\n");
+    int arr3[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    printArray(arr3, size);
+    oper = shaker(arr3, size);
+    printArray(arr3, size);
+    printf("Количество операций: %d\n\n", oper);
 
     return 0;
 }
