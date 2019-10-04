1126e8b44a8dae3e158cd2a097a50eff7d41a5eb
Your Name <you@example.com>
2019-10-04 12:10:04 +0300

    Задание 1

--- main.c
+++ main.c
@@ -1,183 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void DecToBin(int n) {
-    int bit = n % 2;
-    n /= 2;
-    if (n > 0)
-        DecToBin(n);
-    printf("%d", bit);
-}
-
-//Возведение в степень без рекурсии
-int Pow(int a, int b) {
-    int res = 1;
-    for(int i = 0; i < b; i++) {
-        res *= a;
+void fillArray(int* arr, int len) {
+    for (int i = 0; i < len; i++) {
+        arr[i] = rand() % 100;
     }
-    return res;
 }
 
-//Возведение в степень рекурсивно
-int PowR(int a, int b) {
-    int res = 1;    
-    if (b != 0) {
-        b--;
-        res = PowR(a, b);
-        res *= a;
+void printArray(int* arr, int len) {
+    for (int i = 0; i < len; i++) {
+
+        printf("%d ", arr[i]);
     }
-
-    return res;
+    printf("\n");
 }
 
-//Возведение в степень, используя четность
-int Pow2(int a, int b) {
-    int res = 1;
+void swap(int *a, int *b) {
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
 
-    while(b) {
-        if (b % 2 == 0) {
-            a *= a;
-            b /= 2;
-        }
-        else {
-            res *= a;
-            b--;
+//Пузырьковый метод (НЕоптимизированный)
+int bubbleSort(int* arr, int len) {
+    int operations = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len - 1; j++) {
+            operations++;
+
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+
+            }
         }
     }
 
-    return res;
+    return operations;
 }
 
-//Возведение в степень, используя четность рекурсивно
-int PowR2(int a, int b) {
-    int res = 1;
+//Пузырьковый метод (оптимизированный)
+//Оптимизация произведена в двух местах: количество итераций обоих счетчиков
+int bubbleSortOpt(int* arr, int len) {
+    int operations = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - i; j++) {
+            operations++;
 
-    if (b != 0)
-    {
-        if (b % 2 == 0) {
-            b /= 2;
-            res = PowR2(a, b);
-            res *= res;
-
-        }
-        else {
-
-            b--;
-            res = PowR2(a, b);
-            res *= a;
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
         }
     }
 
-    return res;
+    return operations;
 }
 
-//Подсчет количества программ с использованием циклов
-int CountProgrammCycle(int begin, int end) {
-    int count = 0;
+int main(void)
+{
+    //Инициализируем массив
+    int size = 9;
+    int arr1[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    printArray(arr1, size);
 
-    for (int i = begin; i <= end/2; i++) {
-        int p = 0;
-        int tmp = 0;
-        do
-        {
-            tmp = i * Pow(2, p);
-            p++;
-            count++;
-        }
-        while(tmp <= end);
-        count--;
-    }
+    //Сортируем неоптимизированным пузырьковым методом и получаем кол-во операций
+    int oper = bubbleSort(arr1, size);
+    printArray(arr1, size);
+    printf("Количество операций: \%d\n", oper);
 
-    return count;
-}
+    int arr2[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    printArray(arr2, size);
 
-//Подсчет количества программ с использованием рекурсии
-int CountProgrammR(int begin, int end, int p) {
-    int res = 1;
-
-    if (begin <= end/2)
-    {
-        p++;
-
-        int tmp = begin * Pow(2, p - 1);
-
-        if (tmp > end)
-        {
-            begin++;
-            p = 0;
-            res--;
-        }
-
-        res = res + CountProgrammR(begin, end, p);
-    }
-
-    return res;
-}
-
-void Task1() {
-    int n = 0;
-    printf("Введите целое положительное число: ");
-    scanf("%d", &n);
-    printf("Двоичное представление введенного числа: ");
-    DecToBin(n);
-}
-
-void Task2() {
-    int a = 0;
-    int b = 0;
-    printf("Введите целое положительное число: ");
-    scanf("%d", &a);
-    printf("Введите показатель степени: ");
-    scanf("%d", &b);
-
-    printf("Без рекурсии: \n");
-    printf("%d в степени %d равно %d\n", a, b, Pow(a,b));
-
-    printf("Рекурсивно: \n");
-    printf("%d в степени %d равно %d\n", a, b, PowR(a,b));
-
-    printf("Рекурсивно, используя четность степени : \n");
-    printf("%d в степени %d равно %d", a, b, PowR2(a,b));
-}
-
-void Task3() {
-    int begin = 3;
-    int end = 20;
-
-    printf("С использованием массива: \n");
-    printf("Количество программ: %d\n", CountProgrammCycle(begin, end));
-
-    printf("С использованием рекурсии: \n");
-    int p = 0;
-    int count = CountProgrammR(begin, end, p) - 1;
-    printf("Количество программ: %d", count);
-
-}
-
-int main() {
-    //1. Реализовать функцию перевода из 10 системы в двоичную используя рекурсию.
-    printf("Задача 1\n");
-    Task1();
-    printf("\n\n");
-    
-    //2. Реализовать функцию возведения числа a в степень b:
-    //a. без рекурсии;
-    //b. рекурсивно;
-    //c. *рекурсивно, используя свойство чётности степени.
-    printf("Задача 2\n");
-    Task2();
-    printf("\n\n");
-
-    //3. Исполнитель Калькулятор преобразует целое число, записанное на экране
-    //У исполнителя две команды, каждой команде присвоен номер:
-    //1.Прибавь 1
-    //2.Умножь на 2
-    //Первая команда увеличивает число на экране на 1, вторая увеличивает это число в 2 раза.
-    //Сколько существует программ, которые число 3 преобразуют в число 20?
-    //а) с использованием массива;
-    //б) с использованием рекурсии.
-    printf("Задача 3\n");
-    Task3();
-    printf("\n\n");
+    //Сортируем оптимизированным пузырьковым методом
+    oper = bubbleSortOpt(arr2, size);
+    printArray(arr2, size);
+    printf("Количество операций: \%d\n", oper);
 
     return 0;
 }
