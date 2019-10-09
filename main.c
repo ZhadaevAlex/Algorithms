7dcce455ed8f1e715e4466b3a29d14e3fcf479f5
Your Name <you@example.com>
2019-10-09 23:32:20 +0300

    Задание 1

--- main.c
+++ main.c
@@ -1,125 +1,139 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 7
-#define M 7
-#define END 50
+#include <string.h>
 
-int board[N][M];
+#define T char
+//#define SIZE 100
 
-void filling2Array(int n, int m, int arr[n][m], int value) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            arr[i][j] = value;
+//Реализация структуры через массив
+//T Stack[SIZE];
+//int ind = -1;
+
+//void push(char value) {
+//    if (ind < SIZE)
+//        Stack[++ind] = value;
+//    else
+//        printf("Stack overflow");
+//}
+
+//T pop() {
+//    if (ind != -1)
+//        return Stack[ind--];
+//    else {
+//        printf("Stack is empty");
+//        return -1;
+//    }
+//}
+
+//Реализация структуры через односвязный список
+typedef int boolean;
+#define true 1
+#define false 0;
+
+typedef struct Node {
+    T data;
+    struct Node *next;
+} Node;
+
+typedef struct {
+    Node *head;
+    int size;
+} Stack;
+
+boolean push(Stack *st, T value) {
+    Node *temp = (Node*) malloc(sizeof(Node));
+    if (temp == NULL)
+        return false;
+
+    temp->data = value;
+    temp->next = st->head;
+    st->head = temp;
+    st->size++;
+    return true;
 }
 
-void print2Array(int n, int m, int arr[n][m]) {
-    for(int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++)
-            printf("%4d ", arr[i][j]);
-        printf("\n");
+T pop(Stack *st) {
+    if (st->size == 0)
+        return -1;
+
+    Node *temp = st->head;
+    T result = temp->data;
+    st->head = st->head->next;
+    st->size--;
+    free(temp);
+    return result;
+}
+
+//Алгоритм проверки скобок
+//Приоритет скобок такой: ([{}])
+//Cоздаем три стека для каждого вида скобок
+//Посимвольно читаем входную строку и:
+//Если скобка открывающаяся, то в соостветсвующий стек помещаем значение
+//Если скобка закрывающаяся, то извлекаем из стека значение
+//Остальные правила описаны в коде
+boolean checkBrace(char* str) {
+    Stack sRound;
+    Stack sSquare;
+    Stack sFigure;
+    sRound.size = 0;
+    sRound.head = NULL;
+    sSquare.size = 0;
+    sSquare.head= NULL;
+    sFigure.size = 0;
+    sFigure.head = NULL;
+
+    int len = strlen(str);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] == '(') {
+            if (sSquare.size == 0 && sFigure.size == 0)
+                push(&sRound, 1);
+            else
+                return false; //Перед открытием круглой не закрыты квадратная и фигурная
+        }
+        else if (str[i] == ')') {
+            if (sRound.size != 0 && sSquare.size == 0 && sFigure.size == 0)
+                pop(&sRound);
+            else
+                return false; //Перед закрытием круглой не открыта круглая и не закрыты квадратная и фигурная
+        }
+        else if (str[i] == '[') {
+            if (sFigure.size == 0)
+                push(&sSquare, 1);
+            else
+                return false; //Перед открытием квадратной не закрыта фигурная
+        }
+        else if (str[i] == ']') {
+            if (sSquare.size != 0 && sFigure.size == 0)
+                pop(&sSquare);
+            else
+                return false; //Перед закрытием квадратной не открыта квадратная и не закрыта фигурная
+        }
+        else if (str[i] == '{')
+            push(&sFigure, 1);
+        else if (str[i] == '}') {
+            if (sFigure.size != 0)
+                pop(&sFigure);
+            else
+                return false; //Перед закрытием фигурной не закрыта фигурная
+        }
     }
-}
 
-int routes(int x, int y) {
-    if (x == 0 || y == 0) return 1;
-    else return routes(x - 1, y) + routes(x, y - 1);
-}
+    if ((sRound.size != 0) || (sSquare.size != 0) || (sFigure.size != 0))
+        return false; //Не закрыта хотя бы одна из скобок
 
-//Обходим препятствия
-int routesBarrier(int x, int y, int n, int m, int desk[n][m]) {
-    if (desk[x][y] == 0)
-        return 0;
-    else if (x == 0 || y == 0)
-        return 1;
-    else return routesBarrier(x - 1, y, n, m, desk) + routesBarrier(x, y - 1, n, m, desk);
-}
-
-//Варианты хода коня относительно его текущего положения
-int stepX[] = {2, 2, -1, 1, -2, -2, -1, 1};
-int stepY[] = {1, -1, 2, 2, -1, 1, -2, -2};
-
-int searchSolution(int n, int row, int col) {
-    if (n == END)
-        return 1;
-    int i = 0;
-    while(i < 8) {
-        if (row + stepX[i] < 0 || row + stepX[i] > N - 1 || col + stepY[i] < 0 || col + stepY[i] > M - 1) {
-            i++;
-            continue;
-        }
-
-        if (board[row + stepX[i]][col + stepY[i]] == 0) {
-            board[row + stepX[i]][col + stepY[i]] = n;
-
-            row += stepX[i];
-            col += stepY[i];
-
-            if (searchSolution(n + 1, row, col))
-                return 1;
-
-            board[row][col] = 0;
-        }
-        else {
-            i++;
-            continue;
-        }
-    }
-    return 0;
+    return true;
 }
 
 int main(void)
 {
-    //---------- Сделали на уроке ----------
-    printf("Кол-во маршрутов без препятствий\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++)
-            printf("%5d", routes(i, j));
-        printf("\n");
-    }
+    char str[100];
+    printf("Введите математическое выражение:\n");
+    scanf("%s", str);
 
-    printf("\n");
-
-    //-------------------------------------------
-    //1. *Количество маршрутов с препятствиями.
-    //Реализовать чтение массива с препятствием и нахождение количество маршрутов.
-
-    int desk[N][M] = {0};
-
-    printf("Расположение препятствий\n");
-    printf("0 - препятствие\n");
-    printf("1 - нет препятствие\n\n");
-
-    //Формирование массива без припятствий
-    filling2Array(N, M, desk, 1);
-
-    //Формирование препятствий
-    desk[0][3] = 0;
-    desk[0][4] = 0;
-    desk[2][2] = 0;
-    desk[4][1] = 0;
-    desk[4][0] = 0;
-    desk[6][5] = 0;
-
-    //Печать массива с припятствиями
-    print2Array(N, M, desk);
-
-    printf("\n");
-
-    printf("Кол-во маршрутов с препятствиями\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++)
-            printf("%5d", routesBarrier(i, j, N, M, desk));
-        printf("\n");
-    }
-
-    printf("\n");
-
-    //-------------------------------------------
-    //3. ***Требуется обойти конём шахматную доску размером NxM, пройдя через все поля доски по одному разу.
-    printf("Обходим доску конем\n");
-    filling2Array(N, M, board, 0);
-    searchSolution(1, 0, 0);
-    print2Array(N, M, board);
+    printf("Последовательность \%s\n", checkBrace(str) ? "правильная" : "неправильная");
 
     return 0;
 }
+
