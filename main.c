98ebe0c2d0f10b2cc3aefbdddb98bc69f2faf729
Your Name <you@example.com>
2019-10-11 11:46:27 +0300

    Изменен алгоритм и реализация задания 1. Выполнено задание 3.

--- main.c
+++ main.c
@@ -25,7 +25,7 @@
 //    }
 //}
 
-//Реализация структуры через односвязный список
+//Реализация стека через односвязный список
 typedef int boolean;
 #define true 1
 #define false 0;
@@ -64,76 +64,139 @@ T pop(Stack *st) {
     return result;
 }
 
-//Алгоритм проверки скобок
-//Приоритет скобок такой: ([{}])
-//Cоздаем три стека для каждого вида скобок
-//Посимвольно читаем входную строку и:
-//Если скобка открывающаяся, то в соостветсвующий стек помещаем значение
-//Если скобка закрывающаяся, то извлекаем из стека значение
-//Остальные правила описаны в коде
+T peek(Stack *st) {
+    if (st->size == 0)
+        return -1;
+
+    return st->head->data;
+}
+
+//Реализация очереди через двусвязный список
+typedef struct NodeTwo {
+    T data;
+    struct NodeTwo *next;
+    struct NodeTwo *prev;
+
+} NodeTwo;
+
+typedef struct {
+    NodeTwo *head;
+    NodeTwo *tail;
+    int size;
+} Queue;
+
+boolean Enqueue(Queue *que, T value) {
+    NodeTwo *temp = (NodeTwo*) malloc(sizeof(NodeTwo));
+    if (temp == NULL)
+        return false;
+
+    temp->data = value;
+    temp->next = que->head;
+    temp->prev = NULL;
+
+    if (que->size > 0) {
+        que->head->prev = temp;
+    }
+
+    que->head = temp;
+
+    if (que->size == 0) {
+        que->tail = temp;
+    }
+    else if (que->size == 1)
+        que->tail->prev = que->head;
+
+    que->size++;
+
+    return true;
+}
+
+T Dequeue(Queue *que) {
+    if (que->size == 0)
+        return -1;
+
+    NodeTwo *temp = que->tail;
+    T result = temp->data;
+
+    que->tail = que->tail->prev;
+    que->size--;
+    free(temp);
+
+    return result;
+}
+
+//Функция проверки правильности скобок
 boolean checkBrace(char* str) {
-    Stack sRound;
-    Stack sSquare;
-    Stack sFigure;
-    sRound.size = 0;
-    sRound.head = NULL;
-    sSquare.size = 0;
-    sSquare.head= NULL;
-    sFigure.size = 0;
-    sFigure.head = NULL;
+    Stack stack;
+
+    stack.size = 0;
+    stack.head = NULL;
 
     int len = strlen(str);
 
     for (int i = 0; i < len; i++)
     {
-        if (str[i] == '(') {
-            if (sSquare.size == 0 && sFigure.size == 0)
-                push(&sRound, 1);
-            else
-                return false; //Перед открытием круглой не закрыты квадратная и фигурная
-        }
-        else if (str[i] == ')') {
-            if (sRound.size != 0 && sSquare.size == 0 && sFigure.size == 0)
-                pop(&sRound);
-            else
-                return false; //Перед закрытием круглой не открыта круглая и не закрыты квадратная и фигурная
-        }
-        else if (str[i] == '[') {
-            if (sFigure.size == 0)
-                push(&sSquare, 1);
-            else
-                return false; //Перед открытием квадратной не закрыта фигурная
-        }
-        else if (str[i] == ']') {
-            if (sSquare.size != 0 && sFigure.size == 0)
-                pop(&sSquare);
-            else
-                return false; //Перед закрытием квадратной не открыта квадратная и не закрыта фигурная
-        }
-        else if (str[i] == '{')
-            push(&sFigure, 1);
-        else if (str[i] == '}') {
-            if (sFigure.size != 0)
-                pop(&sFigure);
-            else
-                return false; //Перед закрытием фигурной не закрыта фигурная
-        }
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{')
+                push(&stack, str[i]);
+        else if (str[i] == ')' && peek(&stack) == '(')
+                pop(&stack);
+        else if (str[i] == ']' && peek(&stack) == '[')
+                pop(&stack);
+        else if (str[i] == '}' && peek(&stack) == '{')
+                pop(&stack);
+        else
+            continue;
     }
 
-    if ((sRound.size != 0) || (sSquare.size != 0) || (sFigure.size != 0))
-        return false; //Не закрыта хотя бы одна из скобок
+    if (stack.size != 0)
+        return false;
 
     return true;
 }
 
 int main(void)
 {
+    //1. Написать программу, которая определяет,
+    //является ли введенная скобочная последовательность правильной.
+    //Примеры правильных скобочных выражений:
+    //(), ([])(), {}(), ([{}]), неправильных — )(, ())({), (, ])}), ([(]) для скобок [,(,{.
+    printf("Задание 1. Проверка правильности скобок\n");
+
     char str[100];
-    printf("Введите математическое выражение:\n");
+    printf("Введите скобочное выражение:\n");
     scanf("%s", str);
 
-    printf("Последовательность \%s\n", checkBrace(str) ? "правильная" : "неправильная");
+    printf("Последовательность %s\n", checkBrace(str) ? "правильная" : "неправильная");
 
+    printf("\n");
+
+    //2. *Создать функцию, копирующую односвязный список
+    //(то есть создает в памяти копию односвязного списка, без удаления первого списка).
+    Stack stack;
+    stack.size = 0;
+    stack.head = NULL;
+
+    push(&stack, 'a');
+    push(&stack, 'b');
+    push(&stack, 'c');
+
+    //3. *Реализовать очередь на основе двусвязного списка.
+    printf("Задание 3. Реализация очереди\n");
+    Queue q;
+    q.size = 0;
+    q.head = NULL;
+    q.tail = NULL;
+
+    Enqueue(&q, 'a');
+    Enqueue(&q, 'b');
+    Enqueue(&q, 'c');
+    Enqueue(&q, 'd');
+    Enqueue(&q, 'e');
+
+    while (q.size > 0)
+        printf("%c", Dequeue(&q));
+
+    printf("\n");
     return 0;
 }
 
